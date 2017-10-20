6aacc226e9ca2f8327b3c931a376cffb8e0c888b
Fbag96 <franciscobagomes@hotmail.com>
2017-10-20 21:14:28 +0100

    ocommentpa

---
+++ ekfini.py
@@ -0,0 +1,12 @@
+#Implementação do Extended Kalman Filter
+
+import numpy #NumPy is the fundamental package for scientific computing with Python
+
+
+
+#Initial Conditions
+x0 = 0 #initial position
+m = 0 #mean
+sigma = 0.01 #Standard Deviation
+
+#Covariances
